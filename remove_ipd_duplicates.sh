b4cf2f48be28b6cd42463814eaccae6eed99fb10
devel-rajkumar <rajkumar.mandal@koinpark.com>
2025-07-03 11:40:31 +0530

    stable backend

---
+++ remove_ipd_duplicates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Remove duplicate/legacy files for IPDAdmission
+rm -f /home/devel-rajkumar/Documents/hms/hms/src/main/java/com/task/hms/ipd/model/IpdAdmission.java
+rm -f /home/devel-rajkumar/Documents/hms/hms/src/main/java/com/task/hms/ipd/repository/IpdAdmissionRepository.java
+rm -f /home/devel-rajkumar/Documents/hms/hms/src/main/java/com/task/hms/ipd/model/IpdVitals.java
+rm -f /home/devel-rajkumar/Documents/hms/hms/src/main/java/com/task/hms/ipd/repository/IpdVitalsRepository.java
+# Add more rm commands if you find more duplicate files
